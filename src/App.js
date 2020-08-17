0058b2dce5bc95658df1f06dca96ecf0f7bff229
SardorAlijanov <s.alijanov@studentinha.uz>
2020-08-17 10:27:22 +0500

    Value can appear on screen

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Total from './components/total/Total';
 import History from './components/history/History';
 import Operation from './components/operation/Operation';
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      transactions: [],
-      description:'',
-      amount: '',
-    }
+class App extends Component{
+  state = {
+    transactions: [],
+    description:'',
+    amount: '',
+    resultIncomes: 0,
+    resultExpences: 0,
+    totalBalance: 0,
   }
   
   addTransaction = add =>{
@@ -28,15 +28,34 @@ class App extends Component {
     transactions,
     description: '',
     amount:'',
-  });
+  },this.getTotalBalance);
 }
 
   addAmount = e =>{
-    this.setState({amount: e.target.value});
+    this.setState({amount: parseFloat(e.target.value)});
   }
 
   addDescription = e =>{
     this.setState({description: e.target.value});
+  }
+
+  getIncomes = () => this.state.transactions
+    .reduce((accumulator, item) => item.add ? item.amount + accumulator: accumulator,0)
+
+  getExpences = () => this.state.transactions
+  .reduce((accumulator, item) => !item.add ? item.amount + accumulator: accumulator,0)
+
+  getTotalBalance(){
+    const resultIncomes = this.getIncomes();
+    const resultExpences = this.getExpences();
+
+    const totalBalance = resultIncomes - resultExpences;
+
+    this.setState({
+      resultIncomes,
+      resultExpences,
+      totalBalance,
+    })
   }
 
   render(){
@@ -49,7 +68,11 @@ class App extends Component {
   
           <main>
               <div className="container">
-                <Total/>
+                <Total 
+                  resultExpences={this.state.resultExpences}
+                  resultIncomes={this.state.resultIncomes}
+                  totalBalance={this.state.totalBalance}
+                />
   
                 <History transactions={this.state.transactions}/>
   
@@ -59,7 +82,6 @@ class App extends Component {
                   addAmount = {this.addAmount}
                   description = {this.state.description}
                   amount = {this.state.amount}
-                  //addTransaction, addDescription, addAmount, description, amount
                 />
               </div>
           </main>
--- src/components/total/Total.js
+++ src/components/total/Total.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-export default function Total(){
+export default function Total({ resultExpences, resultIncomes, totalBalance }){
     return (
         <section className="total">
             <header className="total__header"> 
                 <h3>Balance</h3>
-                <p className="total__balance">0 $</p>
+                <p className="total__balance">{totalBalance} $</p>
             </header>
             <div className="total__main">
                 <div className="total__main-item total__income">
                     <h4>Incomes</h4>
                     <p className="total__money total__money-income">
-                        +0 $
+                        +{resultIncomes} $
                     </p>
                 </div>
                 <div className="total__main-item total__expenses">
                     <h4>Expense</h4>
                     <p className="total__money total__money-expenses">
-                        -0 $
+                        -{resultExpences} $
                     </p>
                 </div>
             </div>
